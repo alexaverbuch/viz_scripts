24c687301bb822e92ae31cffab4d889103e89aea
Alex Averbuch <alex.averbuch@gmail.com>
2010-04-06 21:46:03 +0200

    ! first commit
    Signed-off-by: Alex Averbuch <alex.averbuch@gmail.com>

---
+++ src/viz/__init__.py
@@ -0,0 +1,4 @@
+from sys import *
+from viz_results import *
+
+viz_results("/home/alex/workspace/graph_cluster_utils/metrics/GML to visualize")
---
+++ src/viz/viz_results.py
@@ -0,0 +1,99 @@
+import os, glob
+from igraph import *
+
+def viz_results(directory, colored=False): 
+    for file in glob.glob(os.path.join(directory, '*.gml')):
+        print "found .gml file:", os.path.basename(file)
+        print "file name:", os.path.splitext(os.path.basename(file))[0]
+        print "file type:", os.path.splitext(file)[1]
+        viz_result_colored(file, os.path.splitext(file)[0])        
+
+def viz_result_colored(gmlFile, imgFile, imgLayout="fr"):
+    print "loading ", gmlFile, "..." 
+
+    g = Graph.Read_GML(gmlFile)
+    print "loading complete" 
+
+    print "configuring visual style..."
+
+    #color_dict = {-1: "black", 0: "blue", 1: "red", 2: "green", 3: "pink", 4: "yellow", 5: "chocolate", 6: "dark orange", 7: "gray", 8: "maroon", 9: "purple", 10: "aqua", 11: "brown", 12: "dark cyan", 13: "dark green", 14: "dark magenta", 15: "fuchsia"}
+    color_dict = {-1: "#000000", 0: "#0000ff", 1: "#ff0000", 2: "#00ff00", 3: "#ffc0cb", 4: "#ffff00", 5: "#d2691e", 6: "#ff8c00", 7: "#a9a9a9", 8: "#b03060", 9: "#9370db", 10: "#00ffff", 11: "#a52a2a", 12: "#008b8b", 13: "#006400", 14: "#8b008b", 15: "#ff00ff"}
+    visual_style = {}
+    print "\tvertex sizes..."
+    visual_style["vertex_size"] = [5] * g.vcount()
+    print "\tvertex labels..."
+    visual_style["vertex_label"] = [""] * g.vcount()
+    print "\tvertex colors..."
+    visual_style["vertex_color"] = [color_dict[color] for color in g.vs["color"]]
+    print "\tedge colors..."
+    visual_style["edge_color"] = [color_dict[color] for color in g.es["color"]] 
+    print "\tedge width..."
+    visual_style["edge_width"] = 1
+    print "\tlayout..."
+    
+    #large graph = "lgl"
+    #forced directed = "fr"     
+    #distributed recursive (large graph) = "drl"     
+    #forced directed = "grid_fr"
+    #circular = "circle"
+    visual_style["layout"] = g.layout(imgLayout)    
+    
+    print "\twindow size..."
+    visual_style["bbox"] = (2000, 2000)
+    print "\twindow margin..."
+    visual_style["margin"] = 20
+    
+    print "configuring visual style complete"
+    
+    print "saving png:" + imgFile + ".png..."
+    plot(g, imgFile + ".png", **visual_style)
+    
+#    print "saving svg:" + imgFile + ".svg..."
+#    plot(g, imgFile + ".svg", **visual_style)
+    
+    print "done"
+    print "---"
+
+def viz_result_uncolored(gmlFile, imgFile, imgLayout="fr"):
+    print "loading " + gmlFile + "..." 
+
+    g = Graph.Read_GML(gmlFile)
+    print "loading complete" 
+
+    print "configuring visual style..."
+
+    visual_style = {}
+    print "\tvertex sizes..."
+    visual_style["vertex_size"] = [5] * g.vcount()
+    print "\tvertex labels..."
+    visual_style["vertex_label"] = [""] * g.vcount()
+    print "\tvertex colors..."
+    visual_style["vertex_color"] = ["#000000"] * g.vcount()
+    print "\tedge colors..."
+    visual_style["edge_color"] = ["#ff0000"] * g.vcount()
+    print "\tedge width..."
+    visual_style["edge_width"] = 1
+    print "\tlayout..."
+    
+    #large graph = "lgl"
+    #forced directed = "fr"     
+    #distributed recursive (large graph) = "drl"     
+    #forced directed = "grid_fr"
+    #circular = "circle"
+    visual_style["layout"] = g.layout(imgLayout)    
+    
+    print "\twindow size..."
+    visual_style["bbox"] = (2000, 2000)
+    print "\twindow margin..."
+    visual_style["margin"] = 20
+    
+    print "configuring visual style complete"
+    
+    print "saving png:" + imgFile + ".png..."
+    plot(g, imgFile + ".png", **visual_style)
+    
+#    print "saving svg:" + imgFile + ".svg..."
+#    plot(g, imgFile + ".svg", **visual_style)
+    
+    print "done"
+    print "---"
