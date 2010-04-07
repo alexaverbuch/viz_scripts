9e7de7b2902b7cc08fd21544d3929b831bfa8802
Alex Averbuch <alex.averbuch@gmail.com>
2010-04-07 20:32:40 +0200

    ~ use same layout when visualizing many graphs from same directory. faster and consistent layout
    Signed-off-by: Alex Averbuch <alex.averbuch@gmail.com>

--- src/viz/__init__.py
+++ src/viz/__init__.py
@@ -1,5 +1,5 @@
 from sys import *
 from viz_results import *
 
-#viz_results("/home/alex/workspace/graph_cluster_utils/metrics/GML to visualize")
-viz_results_seq("/home/alex/workspace/graph_cluster_utils/metrics/GML to visualize")
+#viz_results("/home/alex/workspace/graph_cluster_utils/metrics/forest-fire img sequence big", colored=True)
+viz_results_seq("/home/alex/workspace/graph_cluster_utils/metrics/add20 img sequence big", colored=True)
--- src/viz/viz_results.py
+++ src/viz/viz_results.py
@@ -1,8 +1,29 @@
 import os, glob
 from igraph import *
 
-# Assume .gml files are named as: "description.cluster_count.time_step.gml"
-def viz_results_seq(directory):
+# Assume .EXT files are named as: "description.cluster_count.time_step.EXT"
+def rename_to_seq(directory, ext="gml"):
+    for filePath in glob.glob(os.path.join(directory, '*.' + ext)):
+        file = os.path.basename(filePath)
+        fileName = os.path.splitext(file)[0]
+        fileExtension = os.path.splitext(file)[1]
+        fileNumber = os.path.splitext(fileName)[1].split(".")[1]
+        newFileName = fileNumber.rjust(4, "0")
+        print "found .gml file:", file
+        print "file name:", fileName 
+        print "file extension:", fileExtension 
+        print "file number:", fileNumber 
+        print "new file name:", newFileName
+         
+        old = os.path.join(os.path.dirname(filePath), os.path.basename(filePath))
+        new = os.path.join(os.path.dirname(filePath), newFileName + fileExtension)
+        print "old: ", old
+        print "new: ", new
+        os.rename(old, new)        
+
+# Assume .GML files are named as: "description.cluster_count.time_step.GML"
+def viz_results_seq(directory, colored=False):
+    layout = False
     for filePath in glob.glob(os.path.join(directory, '*.gml')):
         file = os.path.basename(filePath)
         fileName = os.path.splitext(file)[0]
@@ -13,15 +34,29 @@ def viz_results_seq(directory):
         print "file name:", fileName 
         print "file extension:", fileExtension 
         print "file number:", fileNumber 
-        print "new file name:", newFileName 
-        viz_result_colored(filePath, newFileName)        
+        print "new file name:", newFileName
+        imgFilePath = os.path.join(os.path.dirname(filePath), newFileName)
+        if colored == True:         
+            layout = viz_result_colored(filePath, imgFilePath, default_layout=layout)
+        else:
+            layout = viz_result_uncolored(filePath, imgFilePath, default_layout=layout)                
 
-def viz_results(directory, colored=False): 
-    for file in glob.glob(os.path.join(directory, '*.gml')):
-        print "found .gml file:", os.path.basename(file)
-        viz_result_colored(file, os.path.splitext(file)[0])        
+def viz_results(directory, colored=False):
+    layout = False 
+    for filePath in glob.glob(os.path.join(directory, '*.gml')):
+        file = os.path.basename(filePath)
+        fileName = os.path.splitext(file)[0]
+        fileExtension = os.path.splitext(file)[1]
+        print "found .gml file:", file
+        print "file name:", fileName 
+        print "file extension:", fileExtension                         
+        imgFilePath = os.path.join(os.path.dirname(filePath), fileName)
+        if colored == True:         
+            layout = viz_result_colored(filePath, imgFilePath, default_layout=layout)
+        else:
+            layout = viz_result_uncolored(filePath, imgFilePath, default_layout=layout)                
 
-def viz_result_colored(gmlFile, imgFile, imgLayout="fr"):
+def viz_result_colored(gmlFile, imgFile, imgLayout="fr", default_layout=False):
     print "loading ", gmlFile, "..." 
 
     g = Graph.Read_GML(gmlFile)
@@ -33,7 +68,7 @@ def viz_result_colored(gmlFile, imgFile, imgLayout="fr"):
     color_dict = {-1: "#000000", 0: "#0000ff", 1: "#ff0000", 2: "#00ff00", 3: "#ffc0cb", 4: "#ffff00", 5: "#d2691e", 6: "#ff8c00", 7: "#a9a9a9", 8: "#b03060", 9: "#9370db", 10: "#00ffff", 11: "#a52a2a", 12: "#008b8b", 13: "#006400", 14: "#8b008b", 15: "#ff00ff"}
     visual_style = {}
     print "\tvertex sizes..."
-    visual_style["vertex_size"] = [5] * g.vcount()
+    visual_style["vertex_size"] = [10] * g.vcount()
     print "\tvertex labels..."
     visual_style["vertex_label"] = [""] * g.vcount()
     print "\tvertex colors..."
@@ -41,7 +76,7 @@ def viz_result_colored(gmlFile, imgFile, imgLayout="fr"):
     print "\tedge colors..."
     visual_style["edge_color"] = [color_dict[color] for color in g.es["color"]] 
     print "\tedge width..."
-    visual_style["edge_width"] = 1
+    visual_style["edge_width"] = 2
     print "\tlayout..."
     
     #large graph = "lgl"
@@ -49,7 +84,10 @@ def viz_result_colored(gmlFile, imgFile, imgLayout="fr"):
     #distributed recursive (large graph) = "drl"     
     #forced directed = "grid_fr"
     #circular = "circle"
-    visual_style["layout"] = g.layout(imgLayout)    
+    if default_layout == False:
+        default_layout = g.layout(imgLayout)
+        
+    visual_style["layout"] = default_layout
     
     print "\twindow size..."
     visual_style["bbox"] = (2000, 2000)
@@ -66,8 +104,9 @@ def viz_result_colored(gmlFile, imgFile, imgLayout="fr"):
     
     print "done"
     print "---"
+    return default_layout
 
-def viz_result_uncolored(gmlFile, imgFile, imgLayout="fr"):
+def viz_result_uncolored(gmlFile, imgFile, imgLayout="fr", default_layout=False):
     print "loading " + gmlFile + "..." 
 
     g = Graph.Read_GML(gmlFile)
@@ -93,7 +132,10 @@ def viz_result_uncolored(gmlFile, imgFile, imgLayout="fr"):
     #distributed recursive (large graph) = "drl"     
     #forced directed = "grid_fr"
     #circular = "circle"
-    visual_style["layout"] = g.layout(imgLayout)    
+    if default_layout == False:
+        default_layout = g.layout(imgLayout)
+
+    visual_style["layout"] = default_layout
     
     print "\twindow size..."
     visual_style["bbox"] = (2000, 2000)
@@ -110,3 +152,4 @@ def viz_result_uncolored(gmlFile, imgFile, imgLayout="fr"):
     
     print "done"
     print "---"
+    return default_layout
