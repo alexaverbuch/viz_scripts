83dfe579bb36d3b5f81969ba28b77605b23a13cd
Alex Averbuch <alex.averbuch@gmail.com>
2010-04-08 13:54:35 +0200

    + use GIS graph coordinates for layout instead of using layout algorithm
    Signed-off-by: Alex Averbuch <alex.averbuch@gmail.com>

--- src/viz/viz_results.py
+++ src/viz/viz_results.py
@@ -22,7 +22,7 @@ def rename_to_seq(directory, ext="gml"):
         os.rename(old, new)        
 
 # Assume .GML files are named as: "description.cluster_count.time_step.GML"
-def viz_results_seq(directory, colored=False):
+def viz_results_seq(directory, colored=False, coords=False):
     layout = False
     for filePath in glob.glob(os.path.join(directory, '*.gml')):
         file = os.path.basename(filePath)
@@ -36,12 +36,9 @@ def viz_results_seq(directory, colored=False):
         print "file number:", fileNumber 
         print "new file name:", newFileName
         imgFilePath = os.path.join(os.path.dirname(filePath), newFileName)
-        if colored == True:         
-            layout = viz_result_colored(filePath, imgFilePath, default_layout=layout)
-        else:
-            layout = viz_result_uncolored(filePath, imgFilePath, default_layout=layout)                
+        layout = viz_result(filePath, imgFilePath, default_layout=layout, colored=colored, coords=coords)
 
-def viz_results(directory, colored=False):
+def viz_results(directory, colored=False, coords=False):
     layout = False 
     for filePath in glob.glob(os.path.join(directory, '*.gml')):
         file = os.path.basename(filePath)
@@ -51,12 +48,9 @@ def viz_results(directory, colored=False):
         print "file name:", fileName 
         print "file extension:", fileExtension                         
         imgFilePath = os.path.join(os.path.dirname(filePath), fileName)
-        if colored == True:         
-            layout = viz_result_colored(filePath, imgFilePath, default_layout=layout)
-        else:
-            layout = viz_result_uncolored(filePath, imgFilePath, default_layout=layout)                
+        layout = viz_result(filePath, imgFilePath, default_layout=layout, colored=colored, coords=coords)
 
-def viz_result_colored(gmlFile, imgFile, imgLayout="fr", default_layout=False):
+def viz_result(gmlFile, imgFile, imgLayout="fr", default_layout=False, colored=False, coords=False):
     print "loading ", gmlFile, "..." 
 
     g = Graph.Read_GML(gmlFile)
@@ -66,79 +60,48 @@ def viz_result_colored(gmlFile, imgFile, imgLayout="fr", default_layout=False):
 
     #color_dict = {-1: "black", 0: "blue", 1: "red", 2: "green", 3: "pink", 4: "yellow", 5: "chocolate", 6: "dark orange", 7: "gray", 8: "maroon", 9: "purple", 10: "aqua", 11: "brown", 12: "dark cyan", 13: "dark green", 14: "dark magenta", 15: "fuchsia"}
     color_dict = {-1: "#000000", 0: "#0000ff", 1: "#ff0000", 2: "#00ff00", 3: "#ffc0cb", 4: "#ffff00", 5: "#d2691e", 6: "#ff8c00", 7: "#a9a9a9", 8: "#b03060", 9: "#9370db", 10: "#00ffff", 11: "#a52a2a", 12: "#008b8b", 13: "#006400", 14: "#8b008b", 15: "#ff00ff"}
+    
     visual_style = {}
+    
     print "\tvertex sizes..."
     visual_style["vertex_size"] = [10] * g.vcount()
+    
     print "\tvertex labels..."
     visual_style["vertex_label"] = [""] * g.vcount()
+    
     print "\tvertex colors..."
-    visual_style["vertex_color"] = [color_dict[color] for color in g.vs["color"]]
+    if colored == True:        
+        visual_style["vertex_color"] = [color_dict[color] for color in g.vs["color"]]
+    else:
+        visual_style["vertex_color"] = ["#000000"] * g.vcount()
+        
     print "\tedge colors..."
-    visual_style["edge_color"] = [color_dict[color] for color in g.es["color"]] 
+    if colored == True:        
+        visual_style["edge_color"] = [color_dict[color] for color in g.es["color"]]
+    else:
+        visual_style["edge_color"] = ["#ff0000"] * g.vcount()
+          
     print "\tedge width..."
     visual_style["edge_width"] = 2
-    print "\tlayout..."
     
-    #large graph = "lgl"
-    #forced directed = "fr"     
-    #distributed recursive (large graph) = "drl"     
-    #forced directed = "grid_fr"
-    #circular = "circle"
+    print "\tlayout..."
     if default_layout == False:
-        default_layout = g.layout(imgLayout)
-        
+        if coords == True:
+            # default_layout = [[g.vs["lon"][index], g.vs["lat"][index]] for index in range(len(g.vs["lat"]))]                    
+            # temporary fix for the case when reference node has no coordinates
+            default_layout = [[0, 0]] + [[g.vs["lon"][index], g.vs["lat"][index]] for index in range(1, len(g.vs["lat"]))]
+        else:            
+            #large graph = "lgl"
+            #forced directed = "fr"     
+            #distributed recursive (large graph) = "drl"     
+            #forced directed = "grid_fr"
+            #circular = "circle"
+            default_layout = g.layout(imgLayout)        
     visual_style["layout"] = default_layout
     
     print "\twindow size..."
     visual_style["bbox"] = (2000, 2000)
-    print "\twindow margin..."
-    visual_style["margin"] = 20
     
-    print "configuring visual style complete"
-    
-    print "saving png:" + imgFile + ".png..."
-    plot(g, imgFile + ".png", **visual_style)
-    
-#    print "saving svg:" + imgFile + ".svg..."
-#    plot(g, imgFile + ".svg", **visual_style)
-    
-    print "done"
-    print "---"
-    return default_layout
-
-def viz_result_uncolored(gmlFile, imgFile, imgLayout="fr", default_layout=False):
-    print "loading " + gmlFile + "..." 
-
-    g = Graph.Read_GML(gmlFile)
-    print "loading complete" 
-
-    print "configuring visual style..."
-
-    visual_style = {}
-    print "\tvertex sizes..."
-    visual_style["vertex_size"] = [5] * g.vcount()
-    print "\tvertex labels..."
-    visual_style["vertex_label"] = [""] * g.vcount()
-    print "\tvertex colors..."
-    visual_style["vertex_color"] = ["#000000"] * g.vcount()
-    print "\tedge colors..."
-    visual_style["edge_color"] = ["#ff0000"] * g.vcount()
-    print "\tedge width..."
-    visual_style["edge_width"] = 1
-    print "\tlayout..."
-    
-    #large graph = "lgl"
-    #forced directed = "fr"     
-    #distributed recursive (large graph) = "drl"     
-    #forced directed = "grid_fr"
-    #circular = "circle"
-    if default_layout == False:
-        default_layout = g.layout(imgLayout)
-
-    visual_style["layout"] = default_layout
-    
-    print "\twindow size..."
-    visual_style["bbox"] = (2000, 2000)
     print "\twindow margin..."
     visual_style["margin"] = 20
     
