68c500a604bb2d7bfca079b4905d83c1b72260a9
Alex Averbuch <alex.averbuch@gmail.com>
2010-04-13 18:26:43 +0200

    + support for coordinates + time measurement ~ tuning for big mother forker graph viz using coords
    Signed-off-by: Alex Averbuch <alex.averbuch@gmail.com>

--- src/viz/__init__.py
+++ src/viz/__init__.py
@@ -2,4 +2,5 @@ from sys import *
 from viz_results import *
 
 #viz_results("/home/alex/workspace/graph_cluster_utils/metrics/forest-fire img sequence big", colored=True)
-viz_results_seq("/home/alex/workspace/graph_cluster_utils/metrics/add20 img sequence big", colored=True)
+#viz_results_seq("/home/alex/workspace/graph_cluster_utils/metrics/add20 img sequence big", colored=True)
+viz_result("/home/alex/workspace/graph_gen_utils/var/romania-basic.gml", "romania", colored=False, coords=True)
