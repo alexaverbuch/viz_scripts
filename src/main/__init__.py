56c2f33c46e490a0c98b75100bcb39333d4bf18e
Alex Averbuch <alex.averbuch@gmail.com>
2010-05-26 13:34:51 +0200

    + more matplotlib examples
    Signed-off-by: Alex Averbuch <alex.averbuch@gmail.com>

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -5,4 +5,4 @@ from matplotlib_scripts import *
 #dir = "/home/alex/workspace/graph_gen_utils/var"
 #viz_results(dir, colored=True, coords=True, imgW=500, imgH=500, vLabel=False, vSize=50, eWidth=10, eWidthMulti="weight")
  
-example_annotate()
+example_annotate_advanced()
