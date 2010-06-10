f92bcf36cf284d5b0e6c29a32bf263248efa8793
Alex Averbuch <alex.averbuch@gmail.com>
2010-06-10 12:32:15 +0200

    + line, hist & bar generic funs now reusable to compose complex figures
    Signed-off-by: Alex Averbuch <alex.averbuch@gmail.com>

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -6,44 +6,35 @@ from matplotlib_scripts import *
 #dir = "/home/alex/workspace/graph_gen_utils/var"
 #viz_results(dir, colored=True, coords=True, imgW=500, imgH=500, vLabel=False, vSize=50, eWidth=10, eWidthMulti="weight")
  
-#gis_local_op_global_traffic_2('')
-#gis_local_op_global_traffic_2('')
-dropboxDir = "/media/disk/alex/Dropbox/"
+#dropboxDir = "/media/disk/alex/Dropbox/"
+dropboxDir = "/home/alex/Dropbox/"
 csvDir = dropboxDir + "Neo_Thesis/Notes/evaluation results/GIS/CSV Versions/GIS2 GOp/"
 csvName = csvDir + "GIS2_GOp_GTraf.csv"
+columnX = 'Index'
+columnY1 = '2rand (sorted)'
+columnY2 = '2didic (sorted)' 
+columnY3 = '2hard (sorted)'
 
-#example_nothing()
-#example_bar()
-
-bar_from_file('test.txt',
-              'xaxis',
-              [('column1', 'b'), ('column2', 'g')],
-              ['Col 1', 'Col 2'])
-
-#line_from_file_separate(csvName,
-#                          [[('Index', '2hard (sorted)', 'r', '', 'yLabel1', 'Title1')],
-#                           [('Index', 'Global/Local (sorted)', 'g', '', 'yLabel2', ''),
-#                            ('Index', '2hard (sorted)', 'r', '', 'yLabel1', 'Title1')],
-#                           [('Index', '2hard (sorted)', 'r', 'xLabel3', 'yLabel3', 'Title3')]])
-
-#line_from_file(csvName,
-#               'Index',
-#               [('2hard (sorted)', 'r', 'yLabel1'),
-#                ('2rand (sorted)', 'b', 'xLabel2')],
-#                fileName="output.pdf",
-#                title='Title Goes Here',
-#                xLabel='xLabel',
-#                yLabel='yLabel',)
-
-#csvName = "/home/alex/workspace/viz_scripts/src/main/test.txt"
-#column = 'column1'
-#column = '2didic (sorted)'
-#hist_from_file(csvName, column, binCount=100)
-
-#hist_from_file_separate_sym(csvName,
-#                          [[('2hard (sorted)', 100, 'r', 'gray', '', 'yLabel1', 'one', 'Title1')],
-#                           [('Global/Local (sorted)', 100, 'g', 'gray', '', 'yLabel2', 'two', '')],
-#                           [('2hard (sorted)', 100, 'b', 'gray', 'xLabel3', 'yLabel3', 'three', '')]])
+tempCsvName = "test.txt"
+tempColumnX = 'xaxis'
+tempColumnY1 = 'column1'
+tempColumnY2 = 'column2'
 
 
-#(vals, valsBins, barFaceColor, barEdgeColor, axisXLabel, axisYLabel, axisTitle)
+get_hist_from_file = get_hist_from_file(csvName, columnY1, barBinCount=100,
+                                        axisXLabel='', axisTitle='')
+
+get_line_from_file = get_line_from_file(csvName, columnX,
+                                        [(columnY1, 'r', 'yLabel1'),
+                                         (columnY2, 'b', 'yLabel2')],
+                                        axisXLabel='', axisTitle='')
+
+get_bar_from_file = get_bar_from_file(tempCsvName, tempColumnX,
+                                      [(tempColumnY1, 'b'), (tempColumnY2, 'g')],
+                                      ['Col 1', 'Col 2'],
+                                      axisTitle='')
+
+#show_plot(get_bar_from_file)
+show_plots([[get_hist_from_file],
+            [get_line_from_file],
+            [get_bar_from_file]])                          
--- src/main/matplotlib_scripts.py
+++ src/main/matplotlib_scripts.py
@@ -325,10 +325,16 @@ def bar_from_file(csvName, xAxis, yAxes, yLabels,
     for ltext in legend.get_texts():
         pylab.setp(ltext, fontsize=legendFontsize)
     
-#    plt.axis([0, 2000, 0, 100])
-    plt.title(axisTitle)
-    plt.ylabel(axisYLabel, fontsize=axisFontSize)
-    plt.grid(axisGrid)
+##    plt.axis([0, 2000, 0, 100])
+#    plt.title(axisTitle)
+#    plt.ylabel(axisYLabel, fontsize=axisFontSize)
+#    plt.grid(axisGrid)
+##    plt.axis([0, max(csvDict[values]), 0, len(csvDict[values])])
+
+#    ax.axis([0, 2000, 0, 100])
+    ax.set_title(axisTitle)
+    ax.set_ylabel(axisYLabel, fontsize=axisFontSize)
+    ax.grid(axisGrid)
 #    plt.axis([0, max(csvDict[values]), 0, len(csvDict[values])])
 
     plb.savefig(fileName,
@@ -341,3 +347,207 @@ def bar_from_file(csvName, xAxis, yAxes, yLabels,
                 transparent=figTransparent)
     
     show()
+    
+    
+    
+def show_plot(plot_fun,
+              fileName="output.pdf",
+              figDpi=300, figFacecolor='w', figEdgecolor='w', figOrientation='portrait',
+              figPapertype=None, figFormat='pdf', figTransparent=True):
+    
+    fig = plt.figure()
+    
+    plot_fun(fig, 1, 1, 1)
+    
+    plb.savefig(fileName,
+                dpi=figDpi,
+                facecolor=figFacecolor,
+                edgecolor=figEdgecolor,
+                orientation=figOrientation,
+                papertype=figPapertype,
+                format=figFormat, #png, pdf, ps, eps and svg
+                transparent=figTransparent)
+    
+    show()
+    
+def show_plots(plotFuns,
+               fileName="output.pdf",
+               figDpi=300, figFacecolor='w', figEdgecolor='w', figOrientation='portrait',
+               figPapertype=None, figFormat='pdf', figTransparent=True):
+    
+    fig = plt.figure(1)
+    
+    numRows = len(plotFuns)    
+        
+    currentRow = 0
+    
+    for plotFunsRow in plotFuns:
+        currentRow += 1
+        numCols = len(plotFunsRow)
+        figNum = (currentRow - 1) * numCols
+        for plotFun in plotFunsRow:
+            figNum += 1
+            
+            plotFun(fig, numRows, numCols, figNum)
+            
+    plb.savefig(fileName,
+                dpi=figDpi,
+                facecolor=figFacecolor,
+                edgecolor=figEdgecolor,
+                orientation=figOrientation,
+                papertype=figPapertype,
+                format=figFormat, #png, pdf, ps, eps and svg
+                transparent=figTransparent)
+
+    show()
+    
+def get_line_from_file(csvName, xAxis, yAxes,
+                       axisLineWidth=2.0, axisGrid=True, axisLineAntialiased=True, axisFontSize=12, axisColor='k',
+                       axisXLabel='xLabel', axisYLabel='yLabel', axisTitle='axisTitle',
+                       legendFontsize=12, legendAlpha=0.5, legendShadow=False, legendColor='w',
+                       legendFancybox=False, legendPos='upper right'):
+    
+    def do_line_from_file(fig, numRows, numCols, figNum):
+        
+        csvDict = csv_to_dict(csvName)
+
+        ax = fig.add_subplot(numRows, numCols, figNum)
+        
+        legendLines = ()
+        legendLineNames = ()
+        
+        for (yAxis, yColor, axisName) in yAxes:
+            line = plt.plot(csvDict[xAxis], csvDict[yAxis], linewidth=axisLineWidth)
+            plt.setp(line, color=yColor, linewidth=axisLineWidth, antialiased=axisLineAntialiased)
+            legendLines += (line,)
+            legendLineNames += (axisName,)
+            
+        legend = ax.legend(legendLines,
+                           legendLineNames,
+                           legendPos,
+                           shadow=legendShadow,
+                           fancybox=legendFancybox)
+        
+        frame = legend.get_frame()  
+        frame.set_facecolor(legendColor)
+        frame.set_alpha(legendAlpha)
+        
+        for ltext in legend.get_texts():
+            pylab.setp(ltext, fontsize=legendFontsize)
+        
+    #    plt.axis([0, 2000, 0, 100])
+        ax.set_title(axisTitle, fontsize=axisFontSize)
+        ax.set_xlabel(axisXLabel, fontsize=axisFontSize)
+        ax.set_ylabel(axisYLabel, fontsize=axisFontSize)
+        ax.grid(axisGrid)
+        
+    return do_line_from_file
+        
+def get_hist_from_file(csvName, values,
+                       barBinCount=10, barFacecolor='blue', barEdgecolor='gray', barHisttype='bar',
+                       barAlpha=0.75, barAlign='mid', barOrientation='vertical',
+                       axisGrid=True, axisFontSize=12, axisColor='k',
+                       axisXLabel='xLabel', axisYLabel='yLabel', axisName='axisName', axisTitle='Title Goes Here',
+                       legendFontsize=12, legendAlpha=0.5, legendShadow=False, legendColor='w',
+                       legendFancybox=False, legendPos='upper right'):
+    
+    def do_hist_from_file(fig, numRows, numCols, figNum):            
+        
+        csvDict = csv_to_dict(csvName)
+    
+        if fig == False:
+            fig = plt.figure()
+            
+        ax = fig.add_subplot(numRows, numCols, figNum)
+        
+        #histtype = 'bar' 'barstacked' 'step' 'stepfilled'
+        #align = 'left' 'mid' 'right'
+        #orientation: 'horizontal' 'vertical'
+        n, bins, patches = ax.hist(csvDict[values], barBinCount,
+                                   label=axisName,
+                                   normed=0, cumulative=0,
+                                   facecolor=barFacecolor, edgecolor=barEdgecolor,
+                                   alpha=barAlpha, histtype=barHisttype, align=barAlign,
+                                   orientation=barOrientation)
+        
+    #    n, bins, patches = plt.hist([x0, x1, x2], 10, histtype='bar')
+    #    n, bins, patches = plt.hist(x, 50, normed=1, facecolor='green', alpha=0.75)
+            
+        legend = ax.legend(shadow=legendShadow, fancybox=legendFancybox)
+        
+        frame = legend.get_frame()  
+        frame.set_facecolor(legendColor)
+        frame.set_alpha(legendAlpha)
+        
+        for ltext in legend.get_texts():
+            pylab.setp(ltext, fontsize=legendFontsize)
+        
+    #    plt.axis([0, 2000, 0, 100])
+        ax.set_title(axisTitle, fontsize=axisFontSize)
+        ax.set_xlabel(axisXLabel, fontsize=axisFontSize)
+        ax.set_ylabel(axisYLabel, fontsize=axisFontSize)
+        ax.grid(axisGrid)
+    #    plt.axis([0, max(csvDict[values]), 0, len(csvDict[values])])
+    
+    return do_hist_from_file
+
+def get_bar_from_file(csvName, xAxis, yAxes, yLabels,
+                      barEdgecolor='gray', barHisttype='bar', barAlpha=0.75,
+                      barAlign='mid', barOrientation='vertical', barWidth=0.3,
+                      axisGrid=True, axisFontSize=12, axisColor='k',
+                      axisYLabel='yLabel', axisTitle='Title Goes Here',
+                      legendFontsize=12, legendAlpha=0.5, legendShadow=False, legendColor='w',
+                      legendFancybox=False, legendPos='upper right'):
+    
+    def do_bar_from_file(fig, numRows, numCols, figNum):
+            
+        csvDict = csv_to_dict(csvName)
+    
+        ax = fig.add_subplot(numRows, numCols, figNum)
+    
+        n = max([len(csvDict[val]) for (val, clr) in yAxes])
+        ind = np.arange(n)  # the x locations for the groups
+    
+        rectsColl = []
+        barCount = -1    
+        for (yAxis, barColor) in yAxes:
+            barCount += 1
+            
+            rects = ax.bar(ind + (barWidth * barCount), csvDict[yAxis], barWidth,
+                           color=barColor, edgecolor=barEdgecolor, linewidth=None)
+            rectsColl += [rects]
+        
+#    #    ax.set_ylabel(axisYLabel)
+#    #    ax.set_title(axisTitle)
+#        ax.set_xticks(ind + barWidth)
+#        ax.set_xticklabels(csvDict[xAxis])
+    
+        legend = ax.legend([rects[0] for rects in rectsColl], yLabels,
+                           shadow=legendShadow, fancybox=legendFancybox)
+        
+        def autolabel(rects):
+            # attach some text labels (Optional)
+            for rect in rects:
+                height = rect.get_height()
+                ax.text(rect.get_x() + rect.get_width() / 2., 1.05 * height, '%d' % int(height),
+                        ha='center', va='bottom')
+        
+        for rects in rectsColl:
+            autolabel(rects)
+        
+        frame = legend.get_frame()  
+        frame.set_facecolor(legendColor)
+        frame.set_alpha(legendAlpha)
+        
+        for ltext in legend.get_texts():
+            pylab.setp(ltext, fontsize=legendFontsize)
+        
+    #    ax.axis([0, 2000, 0, 100])
+        ax.set_title(axisTitle, fontsize=axisFontSize)
+        ax.set_ylabel(axisYLabel, fontsize=axisFontSize)
+        ax.grid(axisGrid)
+        ax.set_xticks(ind + barWidth)
+        ax.set_xticklabels(csvDict[xAxis])
+    #    plt.axis([0, max(csvDict[values]), 0, len(csvDict[values])])
+
+    return do_bar_from_file
